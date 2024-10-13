94dab6f9dc5ca5dde20042aa038e165358c11985
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-10-13 15:55:21 -0500

    feat: :construction: PC view
    Se empieza a hacer la vista de pc a la pagina web

--- js/myinformation.js
+++ js/myinformation.js
@@ -1,10 +1,11 @@
 const buttons = document.querySelectorAll('.button_container');
 const contentContainer = document.querySelectorAll('.text_information_container');
+const contentContainerComplete = document.querySelectorAll('.text_information_container_complete');
 
 buttons.forEach((button, index) => {
     button.addEventListener('click', function () {
         // Solo funciona para dispositivos móviles, solo permite seleccionar un botón a la vez.
-        if (window.innerWidth < 768) {
+        if (window.innerWidth <= 768) {
             const isSelected = this.id === 'selected';
 
             buttons.forEach((btn, btnIndex) => {
@@ -18,6 +19,29 @@ buttons.forEach((button, index) => {
                 this.id = 'selected';
                 if (contentContainer[index]) {
                     contentContainer[index].id = 'Miguis722';
+                }
+            }
+        }
+    });
+});
+
+buttons.forEach((button, index) => {
+    button.addEventListener('click', function () {
+        // Solo funciona para dispositivos que NO sean moviles
+        if (window.innerWidth > 768) {
+            const isSelected = this.id === 'selected';
+
+            buttons.forEach((btn, btnIndex) => {
+                btn.removeAttribute('id');
+                if (contentContainerComplete[btnIndex]) {
+                    contentContainerComplete[btnIndex].id = 'hidden';
+                }
+            });
+
+            if (!isSelected) {
+                this.id = 'selected';
+                if (contentContainerComplete[index]) {
+                    contentContainerComplete[index].id = 'ThePestBlack';
                 }
             }
         }
