3c5b07cd9e8edebce7f9d798c59aeb424f70d7dd
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-10-14 16:38:39 -0500

    fix: :bug:

--- js/welcome.js
+++ js/welcome.js
@@ -1,10 +1,10 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     const buttonContainer = document.querySelector('.button_container');
+document.addEventListener('DOMContentLoaded', function () {
+    const buttonContainer = document.querySelector('.button_container');
 
-//     if (buttonContainer) {
-//         // Después de 1 segundo, cambia el id a 'Miguis722'
-//         setTimeout(() => {
-//             buttonContainer.id = 'Miguis722';
-//         }, 1000);
-//     }
-// });
+    if (buttonContainer) {
+        // Después de 1 segundo, cambia el id a 'Miguis722'
+        setTimeout(() => {
+            buttonContainer.id = 'Miguis722';
+        }, 1000);
+    }
+});
