d511eff7fbe7a384f94bca8d0e272ef36e8cfac0
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-10-14 16:32:19 -0500

    fix: :bug: Solution
    Se hace la solución final al botón

--- js/welcome.js
+++ js/welcome.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const buttonContainer = document.getElementsByClassName('button_container');
+    const buttonContainer = document.querySelector('.button_container');
 
-    // Después de 1 segundo, cambia el id a 'Miguis722'
-    setTimeout(() => {
-        buttonContainer.id = 'Miguis722';
-    }, 1000);
+    if (buttonContainer) {
+        // Después de 1 segundo, cambia el id a 'Miguis722'
+        setTimeout(() => {
+            buttonContainer.id = 'Miguis722';
+        }, 1000);
+    }
 });
